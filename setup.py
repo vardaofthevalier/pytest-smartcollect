4c5226f4062ec0f6baf1108a9af38150d01024f1
Abigail Hahn <abhahn@microsoft.com>
2018-08-30 20:11:27 -0700

    Added a dependency to setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     long_description=read('README.rst'),
     py_modules=['pytest_smartcollect'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=['pytest>=3.5.0'],
+    install_requires=['pytest>=3.5.0', 'GitPython==2.1.11'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Pytest',
