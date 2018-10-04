f4192a4542eea67b9fd4787dc90bb6782cee980f
Abigail Hahn <abhahn@microsoft.com>
2018-10-04 13:05:19 -0700

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-smartcollect',
-    version='0.1.3',
+    version='0.1.4',
     author='Abigail Hahn',
     author_email='abigail.n.hahn@gmail.com',
     maintainer='Abigail Hahn',
